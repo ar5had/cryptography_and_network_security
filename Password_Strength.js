84f647e27304351bdab79b02979fce85dfbc2048
Arshad Khan <arshdkhn1@gmail.com>
2017-11-06 23:40:41 +0530

    adding password strength program

---
+++ Password_Strength.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt');
+
+const WEAK_PWD = "Weak Password";
+const STRONG_PWD = "Strong Password";
+
+const findPasswordStrength = pwd => {
+	if(pwd.length < 6) return WEAK_PWD;
+
+	const hasUpper = (/[A-Z]/g).test(pwd)
+	const hasLower = (/[a-z]/g).test(pwd);
+	const hasDigit = (/[0-9]/g).test(pwd);
+
+	const distinctDigits = new Set(pwd.split('')).size;
+
+	if(hasUpper && hasLower && hasDigit && (distinctDigits > 4))
+		return STRONG_PWD;
+	else
+		return WEAK_PWD;
+};
+
+prompt.start();
+
+prompt.get('password', (err, { password }) => {
+	if(!err) {
+		console.log(`Password strength: ${findPasswordStrength(password)}`);
+	}
+});
